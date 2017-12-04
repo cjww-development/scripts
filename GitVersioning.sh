ae672d8b885ba03d57fc46c8bf6b77c1d95b4df3
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2017-12-04 21:19:50 +0000

    1. GitVersioning.sh

---
+++ GitVersioning.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+$Project="PROJECT_NAME"
+$Type="MAJOR or MINOR or HOTFIX"
+
+echo "Versioning project $Project with $Type version"
+
+captured_version=$(git describe --abbrev=0)
+
+echo "Current $Project version is $captured_version"
+
+VERSION_BITS=(${captured_version//./ })
+
+VNUM1=${VERSION_BITS[0]}
+VNUM2=${VERSION_BITS[1]}
+VNUM3=${VERSION_BITS[2]}
+
+#To help debug
+echo "$VNUM1 # $VNUM2 # $VNUM3"
+
+$new_version
+
+if [ "$Type" = "major" ]; then
+	new_version="$((VNUM+1)).0.0"
+elif [ "$Type" = "minor" ]; then
+	new_version="$VNUM1.$((VNUM2+1)).0"
+elif [ "$Type" = "hotfix" ]; then
+	new_version="$VNUM1.$VNUM2.$((VNUM3+1))"
+else
+	echo "Unrecognised inc version"
+fi
+
+echo "$Project is now versioned as $new_version"
+echo "running command..."
+
+git tag -a $new_version -m "$new_version $Project"
+git push origin --tags
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# scripts
-Handy scripts
+# Scripts
+Handy scripts...
+
+## GitVersioning.sh
+Gets the last git tag of the current project and increments that version to the next depending on the $Type parameter.
+- Major: Increments the first number by one and sets the other two to zero e.g 3.4.6 -> 4.0.0
+- Minor: Increments the second number by one e.g 3.4.6 -> 3.5.0
+- Hotfix: Increments the third number by one e.g 3.4.6 -> 3.4.7
